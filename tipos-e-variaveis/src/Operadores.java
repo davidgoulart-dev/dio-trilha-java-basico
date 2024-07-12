881e16a0e58d242adaa5210c13279e54e47290d8
davidgoulart-dev <davidgoulart.tae@gmail.com>
2024-07-12 16:25:09 -0300

    brincando com Java Básico

---
+++ tipos-e-variaveis/src/Operadores.java
@@ -0,0 +1,10 @@
+public class Operadores {
+ public static void main(String[] args) {
+    int numero = 5;
+        //x Repetição
+    //numero = numero + 2;
+
+    System.out.println(++ numero);
+
+ }
+}
